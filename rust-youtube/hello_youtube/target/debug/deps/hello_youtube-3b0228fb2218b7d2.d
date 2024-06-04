18160a4dbe7a82ea4b1a8a3c5e32c883ea70ec99
Nihed Nciri <140057840+Nihed2001@users.noreply.github.com>
2024-06-04 15:23:32 +0100

    Delete rust-youtube directory

--- rust-youtube/hello_youtube/target/debug/deps/hello_youtube-3b0228fb2218b7d2.d
+++
@@ -1,5 +0,0 @@
-/home/nihed/rust-youtube/hello_youtube/target/debug/deps/hello_youtube-3b0228fb2218b7d2: src/main.rs
-
-/home/nihed/rust-youtube/hello_youtube/target/debug/deps/hello_youtube-3b0228fb2218b7d2.d: src/main.rs
-
-src/main.rs:
--- rust-youtube/hello_youtube/target/debug/hello_youtube.d
+++
@@ -1,1 +0,0 @@
-/home/nihed/rust-youtube/hello_youtube/target/debug/hello_youtube: /home/nihed/rust-youtube/hello_youtube/src/main.rs
